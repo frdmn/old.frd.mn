896214fbc9beff37e1a40b9f67b5176d5dd9a3c0
Jonas Friedmann <j@frd.mn>
2014-09-25 14:07:31 +0200

    Glitchin' JS

---
+++ assets/js/_glitch.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    setTimeout(function() {
+    $('body').css('background-image','none');
+    $('#freakshow > img').hide();
+    },51700);
+
+    if ($.browser.msie) {
+        $('#freakshow > img').hide();
+        $('#frdmn img:last-child').hide();
+    }
+});
