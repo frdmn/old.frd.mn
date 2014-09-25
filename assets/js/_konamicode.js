0fcdf20e2035cb8181de607b05c72ba088ab480a
Jonas Friedmann <j@frd.mn>
2014-09-25 15:17:28 +0200

    Add konamicode.js

--- assets/js/_konamicode.js
+++ assets/js/_konamicode.js
@@ -1,11 +1,7 @@
-$(document).ready(function(){
-    if ( window.addEventListener ) {
-    var state = 0, konami = [38,38,40,40,37,39,37,39,66,65];
-    window.addEventListener("keydown", function(e) {
-        if ( e.keyCode == konami[state] ) state++;
-        else state = 0;
-        if ( state == 10 )
-        window.location = "http://frd.mn/pr0n";
-        }, true);
-    }
+$('body').konamicode(function() {
+    window.location = "http://asd.de#pr0n";
+});
+
+konamicode(document.body, function() {
+    alert('Mega Man now has Metal Blade, even though he is not from Konami!');
 });
