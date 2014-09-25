2a6f66cd2ec23810a48b96c28b124c66b699f52e
Jonas Friedmann <j@frd.mn>
2014-09-25 15:30:41 +0200

    Remove dummy

--- assets/js/_konamicode.js
+++ assets/js/_konamicode.js
@@ -1,7 +1,3 @@
 $('body').konamicode(function() {
     window.location = "http://asd.de#pr0n";
 });
-
-konamicode(document.body, function() {
-    alert('Mega Man now has Metal Blade, even though he is not from Konami!');
-});
