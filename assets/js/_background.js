618a3aad4500c60eee0eb7298354fe3f7edb8c55
Jonas Friedmann <j@frd.mn>
2014-09-25 13:12:25 +0200

    JS to select a random glitch from glitch pool

---
+++ assets/js/_background.js
@@ -0,0 +1,8 @@
+var amountGlitches = 18,
+    amountKonamiGlitches = 11;
+
+var randomGlitch = (Math.random() * amountGlitches | 0) + 1,
+    randomKonamiGlitch = (Math.random() * amountKonamiGlitches | 0) + 1;
+
+console.log(randomGlitch);
+console.log(randomKonamiGlitch);
